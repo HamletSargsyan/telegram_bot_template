c11e320ca8d49cc0faad120e945714465f9c5913
Hamlet Sargsyan <hamlets849@gmail.com>
2024-01-25 00:40:51 +0400

    Правки

---
+++ helpers/utils.py
@@ -0,0 +1,19 @@
+from typing import Union, Litera
+from datetime import datetime
+
+from telebot.util import antifloot, escape, smart_split
+
+from config import bot, logger, timezone, log_chat_id, log_thread_id
+
+def error_handler(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.exception(e)
+            log(str(e), "error")
+    return wrapper
+
+@error_handler
+def log(message: str, level: Union[Literal["error"], Literal["warn"], Literal["log"], Literal["success"]]):
+    ...
