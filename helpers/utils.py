80e232991d61116a58c2ff3f6985ecdd6fb0669a
Hamlet Sargsyan <hamlets849@gmail.com>
2024-01-25 10:59:09 +0400

    Правки

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,7 +1,7 @@
 from typing import Union, Literal
 from datetime import datetime
 
-from telebot.util import antifloot, escape, smart_split
+from telebot.util import antiflood, escape, smart_split
 
 from config import bot, logger, timezone, log_chat_id, log_thread_id
 
@@ -20,6 +20,24 @@ def error_handler(func):
 @error_handler
 def log(
     message: str,
-    level: Union[Literal["error"], Literal["warn"], Literal["log"], Literal["success"]],
+    level: Union[Literal["error"], Literal["warn"], Literal["info"], Literal["success"]],
 ):
-    ... 
+    emoji_dict = {
+        "error": "🛑",
+        "warn": "⚠️",
+        "info": "ℹ️",
+        "success": "✅"
+    }
+
+    time = datetime.now(timezone)
+    level_ = f"{emoji_dict.get(level)} {level.upper()}"
+
+
+    try:
+        for mess in smart_split(escape(message)):
+            template = (f"{level_}\n\n"
+                        f"{time}\n\n"
+                        f"<pre><code class='language-shell'>{mess}</code></pre>")
+            antiflood(bot.send_message, log_chat_id, template, message_thread_id=log_thread_id)
+    except Exception as e:
+        logger.exception(e)
